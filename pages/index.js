c272c1aaa475858c26c1122a8fbc295d06e65b7d
Sean OReilly <soreilly424@gmail.com>
2021-08-17 11:07:06 -0500

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>Divespots - Best place for divers around the 🌏 🐳 🐠 🦀</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
         {/* <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤿</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐠</text></svg>"
         /> */}
       </Head>
       <main className="flex flex-col items-center justify-center w-full flex-1 text-center">
